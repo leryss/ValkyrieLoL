bf394d7ea541959b3a6058a3d064b77bd38a6b3e
leryss <laur.h_97@yahoo.ro>
2021-07-11 16:10:08 +0300

    Fixed crash related to keybinds & fixed precompiled header settings

--- ProjectValkyrie/ValkyrieDLL/HKey.h
+++ ProjectValkyrie/ValkyrieDLL/HKey.h
@@ -10,5 +10,5 @@ enum HKey {
 static const char* HKeyNames[] = {
 	"None", "Esc", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace", "Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "Enter", "Ctrl", "A", "S", "D", "F", "G",
 	"H", "J", "K", "L", ";", "'", "`", "LShift", "\\", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "RShift", "PrtScrn", "Alt", "Space", "Caps", "F1", "F2",
-	"F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "Num", "Scrl", "Home", "Num8", "PgUp", "NumMinus", "Num4", "Num5", "Num6", "NumPlus", "End", "NumDown", "PgDown", "Insert", "Del", "MouseBtn", "MouseBtn2"
+	"F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "Num", "Scrl", "Home", "Num8", "PgUp", "NumMinus", "Num4", "Num5", "Num6", "NumPlus", "End", "NumDown", "PgDown", "Insert", "Del", "MouseLeftBtn", "MouseRightBtn", "MouseSideBtn", "MouseSideBtn2"
 };
